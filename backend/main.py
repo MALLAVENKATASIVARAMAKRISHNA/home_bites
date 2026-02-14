0c5744966a76388a2c3b12975d2a00c100a8fb56
ramakrihsna <2003ramakrishna@gmail.com>
2026-02-14 20:05:43 +0530

    config: add CORS middleware for cross-origin requests

--- backend/main.py
+++ backend/main.py
@@ -10,9 +10,18 @@ from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
 from fastapi.responses import JSONResponse
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
